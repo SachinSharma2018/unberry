22e6732669b4ab06d0ad9a1bd2468da50b1f97da
Sachin sharma <sachin.sharma@squareboat.com>
2021-04-16 18:49:40 +0530

    ADD: progress function and fixed some alignment issues and add animation in holdup section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import {useEffect} from "react";
 import AppRouter from "./AppRouter";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 // Styles
 import "./styles/App.scss";
 
 function App() {
+  useEffect(() => {
+    AOS.init();
+    AOS.refresh();
+  });
   return (
     <div className="app-container">
       <AppRouter />
--- src/components/home/holdupSection/index.js
+++ src/components/home/holdupSection/index.js
@@ -1,36 +1,72 @@
-import test3 from "../../../assets/images/big-grid.svg";
+import {useState, useEffect} from "react";
+import grid1 from "../../../assets/images/big-grid.svg";
+import grid2 from "../../../assets/images/big-grid2.svg";
 import shape1 from "../../../assets/images/shapes/g1.svg";
 import HorizontalProgress from "../../horizontalProgress";
+import {Controller, Scene} from "react-scrollmagic";
 
 import "./styles.scss";
 
 function HoldupSection() {
+  const [width, setWidth] = useState(0);
+  const [offset, setOffset] = useState(false);
+  useEffect(() => {
+    window.onscroll = () => {
+      var elmnt = document.getElementById("myDIV");
+      var rect = elmnt.getBoundingClientRect();
+      var elemTop = rect.top;
+
+      if (elemTop < 50 && !offset) {
+        setOffset(true);
+      }
+    };
+
+    let widthVal;
+    if (offset) {
+      widthVal = setInterval(() => setWidth(width + 5), 500);
+    }
+    return () => clearInterval(widthVal);
+  }, [width, offset]);
+
   return (
-    <section className="holdup-section-style">
-      <div className="container">
-        <div className="row">
-          <div className="col-sm-12">
-            <h4 className="title2">Hold up one second..</h4>
+    <Controller>
+      <Scene duration={700} pin triggerHook="onLeave">
+        <section className="holdup-section-style" id="myDIV">
+          <div className="container">
+            <div className="row">
+              <div className="col-sm-12">
+                <h4 className="title2">Hold up one second..</h4>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-      <div className="scroll-section">
-        <img src={test3} />
-      </div>
 
-      <div>
-        <HorizontalProgress width="70" />
-      </div>
+          {!(width >= 100) ? (
+            <div>
+              <div className="scroll-section">
+                <img src={grid1} />
+              </div>
+              <div>
+                <HorizontalProgress width={width} />
+              </div>
+            </div>
+          ) : (
+            <div className="animate__animated animate__fadeIn animate__fast">
+              <div className="scroll-section">
+                <img src={grid2} />
+              </div>
+              <div className="content-section">
+                <div className="science-box">
+                  Science-backed games can present multi-dimensional stimuli to
+                  reveal a lot about players cognition and behaviour.
+                </div>
 
-      {/* <div className="content-section">
-        <div className="science-box">
-          Science-backed games can present multi-dimensional stimuli to reveal a
-          lot about players cognition and behaviour.
-        </div>
-
-        <img src={shape1} className="img-style" />
-      </div> */}
-    </section>
+                <img src={shape1} className="img-style" />
+              </div>
+            </div>
+          )}
+        </section>
+      </Scene>
+    </Controller>
   );
 }
 
